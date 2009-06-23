2fc95223992580ac421b1dfd25547021922deb0d
Peter Backlund <github@petrix.se>
2009-06-23 22:43:54 +0200

    Moved django out of the way, played with a simple google charts view

--- main.py
+++ main.py
@@ -1,39 +1,98 @@
-import logging, os
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from models import *
+import time
+from datetime import date,datetime
 
-# Google App Engine imports.
-from google.appengine.ext.webapp import util
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
 
-# Force Django to reload its settings.
-from django.conf import settings
-settings._target = None
+        url = 'http://chart.apis.google.com/chart?chs=600x300&cht=bvs&chbh=a&chds=0,50'
 
-# Must set this env var before importing any part of Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+        parties = Party.all()
+        polls = Poll.all()
 
-import logging
-import django.core.handlers.wsgi
-import django.core.signals
-import django.db
-import django.dispatch.dispatcher
+        pieces = '&chd=t:'
+        labels = '&chl='
+        colors = '&chco='
+        legends = '&chdl='
+        for poll in polls:
+            for party in parties:
+                pieces += str(poll.percentage_of(party)) + ','
+                labels += party.abbreviation + '|'
+                colors += party.color + '|'
+                legends += party.name + '|'
+        pieces += '0.0'
+        labels += 'NONE'
+        colors += '123456'
+        legends += 'NONE'
 
-def log_exception(*args, **kwds):
-  logging.exception('Exception in request:')
 
-# Log errors.
-django.dispatch.dispatcher.connect(
-   log_exception, django.core.signals.got_request_exception)
+        self.response.out.write('<img src="%s" alt="FAIL"/>' % (url + pieces + labels + colors + legends))
 
-# Unregister the rollback event handler.
-django.dispatch.dispatcher.disconnect(
-    django.db._rollback_on_exception,
-    django.core.signals.got_request_exception)
+application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+
+def setup_sample_data():
+    sifo = Institute(name = "SIFO")
+    sifo.put()
+
+    v = Party(name = "Vansterpartiet", abbreviation = "V", color = "#555")
+    v.put()
+
+    s = Party(name = "Socialdemokraterna", abbreviation = "S", color = "#F00")
+    s.put()
+
+    mp = Party(name = "Miljopartiet", abbreviation = "MP", color = "#123")
+    mp.put()
+
+    c = Party(name = "Centerpartiet", abbreviation = "C", color = "#234")
+    c.put()
+
+    fp = Party(name = "Folkpartiet", abbreviation = "FP", color = "#345")
+    fp.put()
+
+    m = Party(name = "Moderaterna", abbreviation = "M", color = "#456")
+    m.put()
+
+    kd = Party(name = "Kristdemokraterna", abbreviation = "KD", color = "#567")
+    kd.put()
+
+    pp = Party(name = "Piratpartiet", abbreviation = "PP", color = "#678")
+    pp.put()
+
+    fi = Party(name = "Feministiskt Initiativ", abbreviation = "FI", color = "#789")
+    fi.put()
+
+    sd = Party(name = "Sverigedemokraterna", abbreviation = "SD", color = "#89A")
+    sd.put()
+
+    ovr = Party(name = "Ovriga", abbreviation = "OVR", color = "#9AB")
+    ovr.put()
+
+    results = [
+            PollingResult(party = s, percentage = 38.0),
+            PollingResult(party = v, percentage = 5.0),
+            PollingResult(party = mp, percentage = 5.0),    # 48
+            PollingResult(party = c, percentage = 8.0),
+            PollingResult(party = fp, percentage = 8.0),
+            PollingResult(party = kd, percentage = 6.0),
+            PollingResult(party = m, percentage = 24.0),    # 46
+            PollingResult(party = fi, percentage = 1.0),
+            PollingResult(party = sd, percentage = 2.0),
+            PollingResult(party = pp, percentage = 3.0),    # 6
+    ]
+    list = []
+    for result in results:
+        result.put()
+        list.append(result.key())
+
+    poll = Poll(publish_date = datetime.today(), institute = sifo, results = list, sample_size = 2000, question_asked = "Vilket parti gillar du?")
+    poll.put()
 
 def main():
-  # Create a Django application for WSGI.
-  application = django.core.handlers.wsgi.WSGIHandler()
+    #setup_sample_data()
+    run_wsgi_app(application)
 
-  # Run the WSGI CGI handler with that application.
-  util.run_wsgi_app(application)
-  
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
--- models.py
+++ models.py
@@ -1,52 +1,49 @@
 import sys
-sys.path.append("/Users/peter/GoogleAppEngineLauncher.app/Contents/Resources/GoogleAppEngine-default.bundle/Contents/Resources/google_appengine")
-sys.path.append("/Users/peter/GoogleAppEngineLauncher.app/Contents/Resources/GoogleAppEngine-default.bundle/Contents/Resources/google_appengine/lib")
-sys.path.append("/Users/peter/GoogleAppEngineLauncher.app/Contents/Resources/GoogleAppEngine-default.bundle/Contents/Resources/google_appengine/lib/yaml/lib")
 
 from google.appengine.ext import db
 
 class Party(db.Model):
-    name = db.StringProperty()
-    abbreviation = db.StringProperty()
-    color = db.StringProperty()
+    name = db.StringProperty(required=True)
+    abbreviation = db.StringProperty(required=True)
+    color = db.StringProperty(required=True)
 
 
 class Institute(db.Model):
-    name = db.StringProperty()
+    name = db.StringProperty(required=True)
 
 
 class PollingResult(db.Model):
-    party = db.ReferenceProperty(Party)
-    percentage = db.FloatProperty()
+    party = db.ReferenceProperty(Party, required=True)
+    percentage = db.FloatProperty(required=True)
 
-    def my_validate(self, errors):
-        if self.percentage > 100.0:
-            errors.add('Percentage must be smaller than or equal to 100.0: ' + self.percentage)
-        if self.percentage < 0.0:
-            errors.add('Percentage must be large than or equal to 0.0: ' + self.percentage)
-        if self.party == None:
-            errors.add('Party is required')
-        return errors
+    #def my_validate(self, errors):
+    #    if self.percentage > 100.0:
+    #        errors.add('Percentage must be smaller than or equal to 100.0: ' + self.percentage)
+    #    if self.percentage < 0.0:
+    #        errors.add('Percentage must be large than or equal to 0.0: ' + self.percentage)
+    #    if self.party == None:
+    #        errors.add('Party is required')
+    #    return errors
 
 
 class Poll(db.Model):
-    publish_date = db.DateTimeProperty()
-    institute = db.ReferenceProperty(Institute)
-    results = db.ListProperty(db.Key)
-    sample_size = db.IntegerProperty()
-    question_asked = db.StringProperty()
+    publish_date = db.DateTimeProperty(required=True)
+    institute = db.ReferenceProperty(Institute, required=True)
+    results = db.ListProperty(db.Key, required=True)
+    sample_size = db.IntegerProperty(required=True)
+    question_asked = db.StringProperty(required=True)
 
-    def my_validate(self, errors):
-        sum = 0.0
-        for result in self.results:
-            sum += result.percentage
-        if sum != 100.0:
-            errors.add('Sum of percentages must be 100.0: ' + sum)
-        return errors
+    #def my_validate(self, errors):
+    #    sum = 0.0
+    #    for result in self.results:
+    #        sum += result.percentage
+    #    if sum != 100.0:
+    #        errors.add('Sum of percentages must be 100.0: ' + sum)
+    #    return errors
     
-    def percentageOf(self, party):
-        for result in self.results:
-            loaded_result = db.get(result)
-            if loaded_result.party.key() == party.key():
-                return loaded_result.percentage
+    def percentage_of(self, party):
+        results = db.get(self.results)
+        for result in results:
+            if result.party.key() == party.key():
+                return result.percentage
         return 0.0
